715ac046b2213e430c0d5b762876a10f3a54ea80
John Williams <jrw+3@mit.edu>
2025-09-04 08:03:45 -0400

    Refactor ESLint config to use recommended settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,2 +1,15 @@
-import next from 'eslint-config-next';
-export default [next()];
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2023,
+      sourceType: 'module',
+      globals: { ...globals.browser, ...globals.node }
+    },
+    rules: {}
+  }
+];
